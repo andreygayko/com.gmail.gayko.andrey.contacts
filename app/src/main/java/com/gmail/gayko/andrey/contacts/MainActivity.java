591db5052323715f3b444f6664cd1f6f4a729499
Andrei Haiko <Andrei.Haiko@ibm.com>
2020-03-02 15:16:49 +0300

    Add 'Delete database' button

--- app/src/main/java/com/gmail/gayko/andrey/contacts/MainActivity.java
+++ app/src/main/java/com/gmail/gayko/andrey/contacts/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,12 +16,23 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView contacts;
     ContactsAdapter contactsAdapter;
+    Button deleteAll;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts_list);
         contacts = findViewById(R.id.rv_contacts);
+        deleteAll = findViewById(R.id.btn_del_db);
+        deleteAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dropTable();
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+            }
+        });
         contacts.setLayoutManager(new LinearLayoutManager(this));
         //fillTable();
         ArrayList<String> conts = getAllContacts();
@@ -27,24 +41,24 @@ public class MainActivity extends AppCompatActivity {
         //dropTable();
     }
 
-    public ArrayList<String> getAllContacts(){
+    public ArrayList<String> getAllContacts() {
         DatabaseHelper db = new DatabaseHelper(this);
         List<Contact> contacts = db.getAllContacts();
         ArrayList<String> names = new ArrayList<>();
-        for(Contact c : contacts){
+        for (Contact c : contacts) {
             names.add(c.getName());
         }
         return names;
     }
 
-    public void fillTable(){
+    public void fillTable() {
         DatabaseHelper db = new DatabaseHelper(this);
         db.addContact(new Contact("name1", "num1", "address1"));
         db.addContact(new Contact("name2", "num2", "address2"));
         db.addContact(new Contact("name3", "num3", "address3"));
     }
 
-    public void dropTable(){
+    public void dropTable() {
         DatabaseHelper db = new DatabaseHelper(this);
         db.dropTable();
     }
