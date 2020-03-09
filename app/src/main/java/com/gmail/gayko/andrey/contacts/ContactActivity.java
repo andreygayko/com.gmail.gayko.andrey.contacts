273f670373dcf990ade388e752cde3a201bf0119
Andrei Haiko <Andrei.Haiko@ibm.com>
2020-03-09 13:43:28 +0300

    Add menu button to contact layout

--- app/src/main/java/com/gmail/gayko/andrey/contacts/ContactActivity.java
+++ app/src/main/java/com/gmail/gayko/andrey/contacts/ContactActivity.java
@@ -3,17 +3,20 @@ package com.gmail.gayko.andrey.contacts;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ContactActivity extends AppCompatActivity {
+public class ContactActivity extends AppCompatActivity  {
 
     private Context context = this;
     private TextView name, phone, address;
+    private int id;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,48 +26,71 @@ public class ContactActivity extends AppCompatActivity {
         name = findViewById(R.id.tv_name);
         phone = findViewById(R.id.tv_phone);
         address = findViewById(R.id.tv_address);
-        Button edit = findViewById(R.id.btn_edit);
-        final Button delete = findViewById(R.id.btn_delete);
+        Button menu = findViewById(R.id.btn_menu);
 
         Intent intent = getIntent();
-        final int id = intent.getIntExtra("id", -1);
+        id = intent.getIntExtra("id", -1);
 
-        edit.setOnClickListener(new View.OnClickListener() {
+        menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(context, EditContactActivity.class);
-                i.putExtra("id", id);
-                startActivityForResult(i, 1);
-            }
-        });
-
-        delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                deleteContact(getContactInfo(id));
-                Intent i = new Intent(context, MainActivity.class);
-                startActivity(i);
-                finish();
+                showPopup(view);
             }
         });
 
         setContactInfo(getContactInfo(id));
     }
 
-    public Contact getContactInfo(int id){
+    public Contact getContactInfo(int id) {
         DatabaseHelper db = new DatabaseHelper(context);
         return db.getContact(id);
     }
 
-    public void setContactInfo(Contact contact){
+    public void setContactInfo(Contact contact) {
         name.setText(contact.getName());
         phone.setText(contact.getPhoneNumber());
         address.setText(contact.getAddress());
     }
 
-    public void deleteContact(Contact contact){
+    public void deleteContact(Contact contact) {
         DatabaseHelper db = new DatabaseHelper(context);
         db.deleteContact(contact);
+    }
+
+    public void edit() {
+        Intent i = new Intent(context, EditContactActivity.class);
+        i.putExtra("id", id);
+        startActivityForResult(i, 1);
+    }
+
+    public void delete() {
+        deleteContact(getContactInfo(id));
+        Intent i = new Intent(context, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(context, v);
+        popup.inflate(R.menu.menu_contact);
+        popup.show();
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.edit:
+                        System.out.println("Edit");
+                        edit();
+                        return true;
+                    case R.id.delete:
+                        delete();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 
     @Override
